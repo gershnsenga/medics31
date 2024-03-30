4dfdd0fc4a9914ed46f5eedfe4bcc9cd704cc0a8
gershomlapaix <nsengiyumv33@gmail.com>
2024-03-30 19:46:00 +0200

    use match instead of if's

--- src/search_disease.py
+++ src/search_disease.py
@@ -14,34 +14,36 @@ def search_by_disease(disease):
 
         while True:
             whatNow = int(input("\n\t\t\t\t\t\t\t --- Which service now :: "))
-            if whatNow == 1:
 
-                results = "SELECT d.disease_name, i.preventions FROM disease d INNER JOIN disease_info i on d.id=i.disease_id WHERE d.disease_name = %s"
-                cursor.execute(results, (disease,))
+            match whatNow:
+                case 1:
 
-                rows = cursor.fetchall()
-                print("\n\n\t\t\t\t\t\t\t\t {} Preventions\n\t\t\t\t\t\t\t\t -----------".format(disease))
-                for row in rows:
-                    disease_name, prevention = row
-                    print(f"\n\t\t\t\t\t\t\t\t | - {prevention}")
-                
-                print("\n")
-                
-            elif whatNow == 2:
-                results = "SELECT d.disease_name, i.preventions FROM disease d INNER JOIN disease_info i on d.id=i.disease_id WHERE d.disease_name = %s"
-                cursor.execute(results, (disease,))
+                    results = "SELECT d.disease_name, i.preventions FROM disease d INNER JOIN disease_info i on d.id=i.disease_id WHERE d.disease_name = %s"
+                    cursor.execute(results, (disease,))
 
-                rows = cursor.fetchall()
-                print("\n\n\t\t\t\t\t\t\t\t {} Treatment \n\t\t\t\t\t\t\t\t -----------".format(disease))
-                for row in rows:
-                    disease_name, treatment = row
-                    print(f"\n\t\t\t\t\t\t\t\t | - {treatment}")
-                
-                print("\n")
-            elif whatNow == 3:
-                break
-            else:
-                print("\n\t\t\t\t\t\t\t------------------------------\n\t\t\t\t\t\t\t| Your choice is not valid.  | \n\t\t\t\t\t\t\t------------------------------\n")
+                    rows = cursor.fetchall()
+                    print("\n\n\t\t\t\t\t\t\t\t {} Preventions\n\t\t\t\t\t\t\t\t -----------".format(disease))
+                    for row in rows:
+                        disease_name, prevention = row
+                        print(f"\n\t\t\t\t\t\t\t\t | - {prevention}")
+                    
+                    print("\n")
+                    
+                case 2:
+                    results = "SELECT d.disease_name, i.preventions FROM disease d INNER JOIN disease_info i on d.id=i.disease_id WHERE d.disease_name = %s"
+                    cursor.execute(results, (disease,))
+
+                    rows = cursor.fetchall()
+                    print("\n\n\t\t\t\t\t\t\t\t {} Treatment \n\t\t\t\t\t\t\t\t -----------".format(disease))
+                    for row in rows:
+                        disease_name, treatment = row
+                        print(f"\n\t\t\t\t\t\t\t\t | - {treatment}")
+                    
+                    print("\n")
+                case 3:
+                    break
+                case _:
+                    print("\n\t\t\t\t\t\t\t------------------------------\n\t\t\t\t\t\t\t| Your choice is not valid.  | \n\t\t\t\t\t\t\t------------------------------\n")
 
 
     else:
--- tables.sql
+++ tables.sql
@@ -53,15 +53,3 @@ CREATE TABLE disease_gender (
     FOREIGN KEY (disease_id) REFERENCES disease(id),
     FOREIGN KEY (gender_id) REFERENCES gender(id)
 );
-
-
-
-
-
-
-
--- Insert query for the second row
-INSERT INTO disease_info (id, disease_id, preventions, treatment) VALUES ('223e4567-e89b-12d3-a456-426614174000', '556e89b7-e89b-12d3-a456-426614174000', 'Prevention measures for Disease 2', 'Treatment details for Disease 2');
-
--- Insert query for the third row
-INSERT INTO disease_info (id, disease_id, preventions, treatment) VALUES ('323e4567-e89b-12d3-a456-426614174000', '656e89b7-e89b-12d3-a456-426614174000', 'Prevention measures for Disease 3', 'Treatment details for Disease 3');
