ceff8657b479079be255c077fde0ff328436a42c
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-04-13 23:27:24 +0200

    disease and prevention

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from src.search_disease import *
 from src.search_gender import *
+from src.disease_and_prevention import *
 from scripts.welcome import *
 from scripts.app_menu import *
 from scripts.talk_to_doc import talkToDoc
@@ -23,6 +24,8 @@ def main():
                 search_by_gender_menu()
             case 4:
                 print("\n\n\t\t\t--- For disease and prevention\n\n")
+                disease = input("\n\t\t\t\t\t Type a disease : ")
+                disease_and_prevention(disease)
             case 5:
                 print("\n\n\t\t\t--- For disease and treatment\n\n")
             case 6:
